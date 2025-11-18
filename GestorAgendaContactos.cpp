d4488f353e4f0c163c8a2ebc18ab3c4c1d0bb49f
Mariangel Somalia <mariasomalia266@gmail.com>
2025-11-18 17:27:24 -0600

    chore: Inicializa proyecto de C++ con Visual Studio.
    - Se crea un nuevo proyecto de C++ con Visual Studio.
    - Se agrega .gitignore para evitar incluir archivos locales.

---
+++ GestorAgendaContactos.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "---------------------------------------------\n\n";
+  cout << "Bienvenid@ al Gestor de Agenda de Contactos!";
+  cout << "\n\n---------------------------------------------";
+  return 0;
+}
